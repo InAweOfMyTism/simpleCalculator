b59bfa214a0c6a3df7a82e73aec9795faaa9911a
InAweofMyTism <inaweofmytism@gmail.com>
2024-09-01 06:51:50 -0400

    add event listener to num keys

--- script.js
+++ script.js
@@ -2,16 +2,64 @@
 
 let num1, num2; //these will be stored as strings
 let operator; //add, subtract, multiply or divide
-let displayStr = 'calculator'; //store value of whatever is on the screen 
+let displayStr = '0'; //store value of whatever is on the screen 
 display();
 
+//function to turn control keys into inputs
+const controlKeys = document.getElementById('controlKeys');
 
+//function to turn number buttons into inputs
+const numberKeys = document.getElementById('numberKeys');
+numberKeys.addEventListener('click', (event) => {
+    console.log(event.target.getAttribute('id'));
+    enterNum(event.target.getAttribute('id'));
+})
+
+//function to run when a number button is clicked
+function enterNum(button) {
+    switch (button) {
+        case 'button1':
+            displayStr += 1;
+            break;
+        case 'button2':
+            displayStr += 2;
+            break;
+        case 'button3':
+            displayStr += 3;
+            break;
+        case 'button4':
+            displayStr += 4;
+            break;
+        case 'button5':
+            displayStr += 5;
+            break;
+        case 'button6':
+            displayStr += 6;
+            break;
+        case 'button7':
+            displayStr += 7;
+            break;
+        case 'button8':
+            displayStr += 8;
+            break;
+        case 'button9':
+            displayStr += 9;
+            break;
+        case 'button0':
+            displayStr += 0;
+            break;
+        default:
+            break;  
+    }
+    display();
+}
 
 
 //display num
     //run whenever a button is pressed
     
 function display() {
+
     let screen = document.getElementById('display');
     screen.innerText = displayStr;
 }
